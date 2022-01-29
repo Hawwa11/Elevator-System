3de3e61ee1032ab096a84e893712658f0c525a74
Hawwa11 <hawwazohair@gmail.com>
2022-01-29 16:22:08 +0800

    Initial commit

---
+++ Elavator_Application/src/elavator_application/Elavator_Application.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package elavator_application;
+
+/**
+ *
+ * @author uSER
+ */
+public class Elavator_Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
