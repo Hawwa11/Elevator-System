487b9ee167388b3847e1785a9448c38c2242de1e
Arif-ai <arifulislam.ai1313@gmail.com>
2022-03-19 14:10:02 +0300

    Updated Files

---
+++ Elavator_Application/src/elavator_application/User.java
@@ -0,0 +1,15 @@
+package elavator_application;
+
+public class User {
+    double weight;
+    int floorGetOn;
+    int floorGetOff;
+    
+    public User(double w, int fGetOn, int fGetOff)
+    {
+            weight = w;
+            floorGetOn = fGetOn;
+            floorGetOff = fGetOff;
+        
+    }
+}
