fd57a8633673f12aece4b91bd7fcaf850c3194f3
Arif-ai <arifulislam.ai1313@gmail.com>
2022-03-19 14:10:10 +0300

    Updated Files

---
+++ Elavator_Application/src/elavator_application/Main.java
@@ -0,0 +1,21 @@
+package elavator_application;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author uSER
+ */
+public class Main {
+    public static void main(String[] args) {
+            SensorThread t1 = new SensorThread();
+            t1.start();
+
+
+    }
+    
+   
+}
